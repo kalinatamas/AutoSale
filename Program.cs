dfeb38f55691412e1c1c977f01d1d2c13b27c7e6
Kalina Tamás <kalinat@kkszki.hu>
2024-09-26 11:03:54 +0200

    All car datas is available

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoSale.Model;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,35 @@ namespace AutoSale
 {
     internal class Program
     {
+        public static Connect conn = new Connect();
+        public static List<Car> cars = new List<Car>();
+        public static void feladat1()
+        {
+            string sql = "SELECT * FROM cars";
+            conn.Connection.Open();
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+            do 
+            {
+                Car car = new Car();
+                car.Id = dr.GetInt32(0);
+                car.Brand = dr.GetString(1);
+                car.Type = dr.GetString(2);
+                car.License = dr.GetString(3);
+                car.Date = dr.GetInt32(4);
+            } 
+            
+            while (dr.Read());
+            conn.Connection.Close();
+        }
         static void Main(string[] args)
         {
+            feladat1();
+            foreach (var item in cars)
+            {
+                Console.WriteLine($"Márka: {item.Brand}, Azonosító: {item.License}");
+            }
         }
     }
 }
